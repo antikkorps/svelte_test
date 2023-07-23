865eb5b358752f6ce16025fe02449c5e6f7bf868
Franck <franckvienot7@gmail.com>
2023-07-24 00:09:42 +0200

    added flowbite and some components in order to test

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,12 @@
+<script>
+	import SideBar from '../SideBar.svelte';
+</script>
+
 <svelte:head>
 	<title>Dashboard</title>
 	<meta name="description" content="Your dashboard" />
 </svelte:head>
-
+<SideBar />
 <div class="text-column">
 	<h1>This is your dashboard</h1>
 </div>
